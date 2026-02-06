e1e80f6ddcf452b4af2f0a88022eb93eeec56f24
preciz <barna.kovacs@tutanota.com>
2026-02-06 12:30:22 +0100

    Simplify model for faster inference

--- lib/bin_class/model.ex
+++ lib/bin_class/model.ex
@@ -1,54 +1,14 @@
 defmodule BinClass.Model do
   def build(vocab_size, opts \\ []) do
     embedding_size = Keyword.get(opts, :embedding_size, 64)
-    conv_filters = Keyword.get(opts, :conv_filters, 32)
-    dropout_rate = Keyword.get(opts, :dropout_rate, 0.5)
+    conv_filters = Keyword.get(opts, :conv_filters, 128)
+    dropout_rate = Keyword.get(opts, :dropout_rate, 0.2)
 
-    input = Axon.input("input")
-
-    embedding =
-      input
-      |> Axon.embedding(vocab_size, embedding_size)
-      |> Axon.spatial_dropout(rate: 0.1)
-      |> Axon.layer_norm()
-
-    # Standard branches: Kernels [3, 5, 7]
-    standard_branches =
-      for kernel_size <- [3, 5, 7] do
-        conv =
-          embedding
-          |> Axon.conv(conv_filters, kernel_size: kernel_size, activation: :relu, padding: :same)
-
-        max_pool = Axon.global_max_pool(conv)
-        avg_pool = Axon.global_avg_pool(conv)
-
-        [max_pool, avg_pool]
-      end
-      |> List.flatten()
-
-    # Dilated branches: Kernel 3 with dilation [2, 3]
-    dilated_branches =
-      for dilation <- [2, 3] do
-        conv =
-          embedding
-          |> Axon.conv(conv_filters,
-            kernel_size: 3,
-            kernel_dilation: dilation,
-            activation: :relu,
-            padding: :same
-          )
-
-        max_pool = Axon.global_max_pool(conv)
-        avg_pool = Axon.global_avg_pool(conv)
-
-        [max_pool, avg_pool]
-      end
-      |> List.flatten()
-
-    Axon.concatenate(standard_branches ++ dilated_branches)
-    |> Axon.flatten()
-    |> Axon.layer_norm()
+    Axon.input("input")
+    |> Axon.embedding(vocab_size, embedding_size)
     |> Axon.dropout(rate: dropout_rate)
+    |> Axon.conv(conv_filters, kernel_size: 3, activation: :relu)
+    |> Axon.global_max_pool()
     |> Axon.dense(64, activation: :relu)
     |> Axon.dropout(rate: dropout_rate)
     |> Axon.dense(2, activation: :softmax)
